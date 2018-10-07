8a3360e738c558bda5a1c7c656fb50d1154e80aa
Stefan Delibas <delibas.stefan@gmail.com>
2018-10-07 11:15:05 +0300

    home component and routing /test

--- TestWebApp/TestWebApp/webapp/src/app/app.module.ts
+++ TestWebApp/TestWebApp/webapp/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+
+const appRoutes: Routes = [
+  { path: 'test', component: HomeComponent }
+ 
+];
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
