4283e4a7ba1233b2cc31eadbdd2faa2381331325
Stefan Delibas <delibas.stefan@gmail.com>
2018-10-07 03:52:48 +0300

    test app .NET Core API. requests working, no frontend, stable

---
+++ TestWebApp/TestWebApp/Data/WorkoutsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestWebApp.Models
+{
+    public class WorkoutsContext : DbContext
+    {
+        public WorkoutsContext (DbContextOptions<WorkoutsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TestWebApp.Models.Workout> Workout { get; set; }
+    }
+}
