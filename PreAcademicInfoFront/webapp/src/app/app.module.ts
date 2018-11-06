85e9a45da190b1b1ae3136f12dfe6ab81292f38e
DiaNasui <nasuidiana9@gmail.com>
2018-11-06 16:49:29 +0200

    Create the login route

--- PreAcademicInfoFront/webapp/src/app/app.module.ts
+++ PreAcademicInfoFront/webapp/src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MaterialModule} from "./material";
+import { LoginComponent } from './signin/login/login.component';
+import { SigninModule } from './signin/signin.module';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: '/login'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    SigninModule
   ],
   providers: [],
   bootstrap: [AppComponent]
