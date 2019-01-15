a3864773c1a69b6b324dfb29dd64eed9e27354de
Stefan Delibas <delibas.stefan@gmail.com>
2019-01-16 00:39:09 +0200

    new server added with new classes wihtout circular dependecies

---
+++ AcademicInfoServer/AcademicInfoServer/Models/Faculty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademicInfoServer.Models
+{
+    public class Faculty
+    {
+        [Required, Key]
+        public Int32 Id { get; set; }
+
+        [Required]
+        public String NumeUniveristate { get; set; }
+
+        [Required]
+        public String Nume { get; set; }
+
+        [Required]
+        public String Adresa { get; set; }
+
+        public ICollection<Department> Departments { get; set; }
+    }
+}
