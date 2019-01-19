6673e158d05d96602705d10288e63d2ff6d5e438
Stefan Delibas <delibas.stefan@gmail.com>
2019-01-19 22:18:15 +0200

    payment change

--- PreAcademicInfoFront/webapp/src/app/student/payments/payments.component.ts
+++ PreAcademicInfoFront/webapp/src/app/student/payments/payments.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
 import { MatSelect } from '@angular/material';
+import { StudentService } from "../student.service";
+
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
@@ -8,13 +10,10 @@ import { MatSelect } from '@angular/material';
 })
 export class PaymentsComponent implements OnInit {
 
-  semestre = [
-    {value: '1', viewValue: 'semestrul 1'},
-    {value: '2', viewValue: 'semestrul 2'},
-    {value: '3', viewValue: 'semestrul 3'},
-    {value:'4', viewValue:'semestrul 4'},
-    {value:'5', viewValue:'semestrul 5'},
-    {value:'6', viewValue:'semestrul 6'}
+  ani = [
+    {value: '1', viewValue: 'Anul 1'},
+    {value: '2', viewValue: 'Anul 2'},
+    {value: '3', viewValue: 'Anul 3'}
     ];
 
   handler: any;
@@ -26,12 +25,26 @@ export class PaymentsComponent implements OnInit {
   numeTransactie: string;
   tipTranzactie: string;
   inTranse : boolean;
-  constructor() { }
+  selectedMaterie: string;
+  materii = [];
+
+  constructor(private studentService:StudentService) { }
 
   ngOnInit() {
     this.numeTransactie = "Plata semestrul 1";
     this.tipTranzactie = "Plata integrala";
     this.inTranse = false;
+   
+    //load materile predate de un teacher
+    this.studentService.getMaterii(localStorage.getItem('username'))
+    .subscribe(data => 
+      { console.log(data);
+        for (var _i = 0; _i < data.length; _i++)
+        {
+          this.materii.push(data[_i]);
+        }
+      }
+      );
   }
   openCheckout() {
     this.numeTransactie = "Plata " + this.selectedSemestru;
@@ -47,10 +60,27 @@ export class PaymentsComponent implements OnInit {
       key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
       locale: 'auto',
       token: function (token: any) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
+        localStorage['username'];
       }
     });
+  }
+    openCheckout2() {
+      this.numeTransactie = "Plata restanta"+this.selectedMaterie;
+      if(this.inTranse == false)
+      {
+        this.tipTranzactie = "Plata intgrala";
+        this.amount =this.fixedamount
+      }
+      else{
+        this.amount = 360;
+      }
+      var handler = (<any>window).StripeCheckout.configure({
+        key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
+        locale: 'auto',
+        token: function (token: any) {
+          localStorage['username'];
+        }
+      });
 
     handler.open({
       name: this.numeTransactie,
