ce3bf18f767865e15b6e8c1a80b6658f6f911178
Radissan27 <radu.udar27@yahoo.com>
2019-01-19 16:25:06 +0200

    deleted PopulateDatabase from TeacherControler

--- AcademicInfoServerEF22/AcademicInfoServerEF22/Controllers/TeachersController.cs
+++ AcademicInfoServerEF22/AcademicInfoServerEF22/Controllers/TeachersController.cs
@@ -24,41 +24,6 @@ namespace AcademicInfoServerEF22EF22.Controllers
         public TeachersController(AcademicInfoContext context)
         {
             _context = context;
-            PopulateDatabase();
-        }
-
-        public void PopulateDatabase()
-        {
-            //Try to retrieve the Teacher with the Username 'tzutzu'
-            Teacher t = _context.Teacher.Where(teacher => teacher.Username.Equals("tzutzu")).FirstOrDefault();
-
-            //If he is not present into the DB, then it means we must add him
-            if (t == null)
-            {
-                //Generate salt
-                byte[] saltNumber = new byte[10];
-                rngCsp.GetBytes(saltNumber);
-                String saltString = System.Text.Encoding.Default.GetString(saltNumber);
-
-                //Encrypt password
-                String password = BCrypt.Net.BCrypt.HashPassword(saltString + "pass");
-
-                //Add the student 'andi'
-                _context.Teacher.Add(new Teacher()
-                {
-                    Username = "tzutzu",
-                    Email = "tzutzu@cs.ubbcluj.ro",
-                    NumarTelefon = "0711111128",
-                    Nume = "Suciu",
-                    Prenume = "Dan Mircea",
-                    UserType = UserType.TEACHER,
-                    Password = password,
-                    Salt = saltString
-                });
-
-                //Commit changes to DB
-                _context.SaveChanges();
-            }
         }
 
         // GET: api/Teachers
